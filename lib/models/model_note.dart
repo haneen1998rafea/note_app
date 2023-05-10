4a464d695bf46bc5cd306bd5e7934be443c7d83b
rafea_haneen <rafeahaneen@gmail.com>
2023-05-10 17:44:22 +0000

    add hive package

--- lib/models/model_note.dart
+++ lib/models/model_note.dart
@@ -1,1 +1,11 @@
-class ModelNote {}
+class ModelNote {
+  final String title;
+  final String subTitle;
+  final String data;
+  final int color;
+  ModelNote(
+      {required this.title,
+      required this.subTitle,
+      required this.data,
+      required this.color});
+}
